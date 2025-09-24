bc4b986d4bb3b233f44b207ca110b49e5c30de6b
Mohamed Saber <mmms77990@gmail.com>
2025-09-24 12:45:06 +0300

    Updates

--- Infrastructure/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ Infrastructure/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b459498547e6b293a9ec43548045e4ebf47cab53")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
